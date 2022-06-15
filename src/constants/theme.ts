c8f8526a0b1ef25a9728703634a4ea78dfc3f36b
Lampros Barmpounis <labros_b@hotmail.com>
2022-06-15 09:41:47 +0300

    Models and theme implemented

---
+++ src/constants/theme.ts
@@ -0,0 +1,27 @@
+enum Color {
+  Highest = '#3bb7b0',
+  High = '#68d393',
+  Moderate = '#ffda00',
+  Slight = '#ffb379',
+  Minimum = '#ff8787',
+  Neutral = '#ffff',
+  Fade = '#f1edfa',
+  Checked = '#d3cede',
+  PrimaryTypography = '#19345d',
+}
+
+enum Font {
+  Default = 'Montserrat',
+  Italic = 'Montserrat-Italic',
+  Medium = 'Montserrat-Medium',
+  SemiBold = 'Montserrat-SemiBold',
+  Bold = 'Montserrat-Bold',
+}
+
+enum FontSize {
+  Default = '13px',
+  Medium = '15px',
+  Large = '24px',
+}
+
+export { Color, Font, FontSize };
---
+++ src/models/mood.ts
@@ -0,0 +1,16 @@
+enum MoodValue {
+  VeryBad = '1',
+  Bad = '2',
+  Moderate = '3',
+  Good = '4',
+  VeryGood = '5',
+}
+
+enum MoodType {
+  Meh = 'Meh',
+  Content = 'Content',
+  Happy = 'Happy',
+  Frustrated = 'Frustrated',
+}
+
+export { MoodValue, MoodType };
